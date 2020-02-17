720980f0ae4904024c10e3c059ddf67d58d17dbe
Qus <qus1225@gmail.com>
2020-02-18 00:00:13 +0900

    Dummy DB 정리

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,49 @@
-export const people = [
+let movies = [
+  {
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1
+  },
   {
     id: 1,
-    name: "Qus",
-    age: 32,
-    gender: "male"
+    name: "Avengers - The new one",
+    score: 8
   },
   {
     id: 2,
-    name: "My",
-    age: 29,
-    gender: "female"
+    name: "The Godfather I",
+    score: 99
   },
   {
     id: 3,
-    name: "Chang",
-    age: 17,
-    gender: "male"
+    name: "Logan",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,13 @@
 // Query를 처리하는 Resolver를 정의 (실제 DB를 조회)
 
-import { people, getById } from "./db";
+import { getMovies, getById } from "./db";
 
 // schema에서 정의된 opertion을 해결(resolve)하는 역할
 const resolvers = {
   // db를 조회하는 쿼리 역할
   Query: {
-    people: () => people,
-    person: (_, { id }) => {
-      return getById(id);
-    }
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id)
   }
 };
 
