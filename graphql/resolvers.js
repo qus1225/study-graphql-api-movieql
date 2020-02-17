5184ed082bb2cf844c41bb435129790f762b7de1
Qus <qus1225@gmail.com>
2020-02-18 00:23:10 +0900

    Mutation - Delete 추가

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,6 @@
 // Query를 처리하는 Resolver를 정의 (실제 DB를 조회)
 
-import { getMovies, getById, addMovie } from "./db";
+import { getMovies, getById, addMovie, deleteMovie } from "./db";
 
 // schema에서 정의된 opertion을 해결(resolve)하는 역할
 const resolvers = {
@@ -9,7 +9,8 @@ const resolvers = {
     movie: (_, { id }) => getById(id)
   },
   Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score)
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id)
   }
 };
 
