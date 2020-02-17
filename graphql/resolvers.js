a2a77df5d56f60c0240f88d24e0b8e0b5d9ad2be
Qus <qus1225@gmail.com>
2020-02-18 00:13:52 +0900

    Mutation - Create 추가

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,15 @@
 // Query를 처리하는 Resolver를 정의 (실제 DB를 조회)
 
-import { getMovies, getById } from "./db";
+import { getMovies, getById, addMovie } from "./db";
 
 // schema에서 정의된 opertion을 해결(resolve)하는 역할
 const resolvers = {
-  // db를 조회하는 쿼리 역할
   Query: {
     movies: () => getMovies(),
     movie: (_, { id }) => getById(id)
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score)
   }
 };
 
